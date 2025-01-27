14c6a588c1fc6aa4553e0504d39d8ff3a09e0747
willmcroberts <williammcroberts15@gmail.com>
2025-01-27 17:16:52 +0000

    Started numberGuess.cpp

---
+++ numeberGuesser.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <random>
+
+int main() {
+
+	std::string userName;
+
+	std::cout << "Please enter your name: ";
+	std::cin >> userName;
+	std::cout << "Hello " << userName << ", let's play a game" << std::endl;
+} // end main
